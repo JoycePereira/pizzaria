83cc3fa9fab6de271f6daea0010b08abda6bc7f7
JoycePereira <53274354+JoycePereira@users.noreply.github.com>
2019-10-10 15:11:10 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,8 @@
+<?php
+$conexao = mysqli_connect("localhost","id11099541_projmobile","projmobile","id11099541_projmobile");
+//onde esta o banco   usuario do banco   senha   nome do banco
+$sabor = $_GET['sabor'];
+$valor = $_GET['valor'];
+$query = "insert into tb_pizza values (null,'Portuguesa',10.00)";
+mysqli_query($conexao,$query);
+echo "Registro salvo com sucesso!";
---
+++ lista.php
@@ -0,0 +1,17 @@
+<?php
+$conexao = mysqli_connect("localhost","id11099541_projmobile","projmobile","id11099541_projmobile");
+$query = "SELECT * FROM tb_pizza order by ds_sabor";
+$result = mysqli_query($conexao,$query);
+$registro = array(
+    'pizzas'=>array()
+);
+$i=0;
+while($linha = mysqli_fetch_assoc($result)){
+    $registro['pizzas'][$i] = array(
+        'codigo' => $linha['cd_pizza'],
+        'sabor' => $linha['ds_sabor'],
+        'valor' => $linha['vl_pizza'],
+    );
+    $i++;
+}
+echo json_encode($registro);
